fd9b1727f5d92f5234fa5ea684841c2856de51b8
Matheus Germano da Costa <dev.mgermano@gmail.com>
2023-04-07 14:39:25 -0300

    :sparkles: feat: database connection

--- server/index.ts
+++ server/index.ts
@@ -1,5 +1,13 @@
-const express = require('express');
+import express from 'express';
+import { createConnection } from 'typeorm';
+import { config } from './ormconfig';
 
 const app = express();
+
+
+(async () => {
+  const connection = await createConnection(config);
+  console.log(`Connected to database ${connection.options.database}`);
+})();
 
 app.listen(3000, () => console.log('🚀 Server running on http://localhost:3000'));
---
+++ server/ormconfig.ts
@@ -0,0 +1,16 @@
+import { ConnectionOptions } from 'typeorm';
+require('dotenv').config();
+
+
+export const config: ConnectionOptions = {
+  type: 'postgres',
+  host: String(process.env.DATABASE_HOST),
+  port: Number(process.env.DATABASE_PORT),
+  username: String(process.env.DATABASE_USERNAME),
+  password: String(process.env.DATABASE_PASSWORD),
+  database: String(process.env.DATABASE_NAME),
+  entities: ["src/models/**/*.ts"],
+  migrations: ["src/migrations/**/*.ts"],
+  subscribers: ["src/subscribers/**/*.ts"],
+  synchronize: true, // set to false for production
+};
