a0fdfb0548baa8739ea49fefb9aa5d2a6b9ddcce
Matheus Germano da Costa <dev.mgermano@gmail.com>
2023-04-10 15:32:03 -0300

    :sparkles: feat: User entity

---
+++ server/src/models/User.ts
@@ -0,0 +1,22 @@
+import { Entity, PrimaryGeneratedColumn, Column } from "typeorm";
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column('varchar')
+  name: string;
+
+  @Column('varchar', { unique: true })
+  email: string;
+
+  @Column('varchar')
+  password: string;
+
+  @Column('varchar')
+  phone_number: string;
+
+  @Column('text')
+  profile_avatar: string;
+}
