5676fc66cbbd051fced7bd3bf409a172f5aa3749
Matheus Germano da Costa <dev.mgermano@gmail.com>
2023-06-01 09:41:10 -0300

    :sparkles: feat: category and ingredient model

--- server/src/models/Recipe.ts
+++ server/src/models/Recipe.ts
@@ -3,31 +3,31 @@ import { User } from './User';
 
 @Entity()
 export class Recipe {
-	@PrimaryGeneratedColumn('uuid')
-	recipeId: string;
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
 
-	@Column('uuid')
-	creatorId: string;
+  @Column('uuid')
+  creator_id: string;
 
-	@ManyToOne(() => User, { onDelete: 'CASCADE' })
-	@JoinColumn({ name: 'creatorId' })
-	creator: User;
+  @ManyToOne(() => User, { onDelete: 'CASCADE' })
+  @JoinColumn({ name: 'creator_id' })
+  creator: User;
 
-	@Column('varchar')
-	name: string;
+  @Column('varchar')
+  name: string;
 
-	@Column('varchar')
-	description: string;
+  @Column('varchar')
+  description: string;
 
-	@Column('text')
-	preparationMethod: string;
+  @Column('text')
+  preparation_method: string;
 
-	@Column('int')
-	preparationTime: number;
+  @Column('int')
+  preparation_time: number;
 
-	@Column('decimal')
-	servings: number;
+  @Column('decimal')
+  servings: number;
 
-	@Column('date')
-	createdAt: Date;
+  @Column('date')
+  created_at: Date;
 }
--- server/src/models/User.ts
+++ server/src/models/User.ts
@@ -2,21 +2,21 @@ import { Entity, PrimaryGeneratedColumn, Column, OneToMany } from 'typeorm';
 
 @Entity()
 export class User {
-	@PrimaryGeneratedColumn('uuid')
-	id: string;
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
 
-	@Column('varchar')
-	name: string;
+  @Column('varchar')
+  name: string;
 
-	@Column('varchar', { unique: true })
-	email: string;
+  @Column('varchar', { unique: true })
+  email: string;
 
-	@Column('varchar')
-	password: string;
+  @Column('varchar')
+  password: string;
 
-	@Column('varchar')
-	phone_number: string;
+  @Column('varchar')
+  phone_number: string;
 
-	@Column('text')
-	profile_avatar: string;
+  @Column('text', { nullable: true })
+  profile_avatar: string;
 }
