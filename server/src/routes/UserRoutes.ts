3911791e229b6f5d98720e52688ebe0441db59f8
Matheus Germano da Costa <dev.mgermano@gmail.com>
2023-06-03 14:26:32 -0300

    :recycle: refactor: using a user controller factory instead of creating a new object

--- server/src/routes/UserRoutes.ts
+++ server/src/routes/UserRoutes.ts
@@ -1,10 +1,10 @@
 /* eslint-disable @typescript-eslint/no-misused-promises */
 import { Router } from 'express'
-import { UserController } from '../controllers/UserController'
+import { UserControllerFactory } from '../factories/UserControllerFactory'
 
 const userRouter = Router()
 
-const userController = new UserController()
+const userController = UserControllerFactory.generate()
 
 userRouter.post('/users/sign-up', userController.signUp)
 userRouter.post('/users/sign-in', userController.signIn)
