e35f40cdd4b557a519c17ca3c6693339f718a6ae
Matheus Germano da Costa <dev.mgermano@gmail.com>
2023-06-03 14:27:06 -0300

    lint rule

--- server/.eslintrc.js
+++ server/.eslintrc.js
@@ -14,6 +14,7 @@ module.exports = {
   rules: {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/strict-boolean-expressions': 'off',
-    '@typescript-eslint/no-invalid-void-type': 'off'
+    '@typescript-eslint/no-invalid-void-type': 'off',
+    '@typescript-eslint/no-extraneous-class': 'off'
   }
 }
